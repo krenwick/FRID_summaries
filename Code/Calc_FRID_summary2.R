e6f8ba1e488bafc7840534936c5ce719cae2234e
krenwick <katie.renwick@gmail.com>
2019-05-20 15:54:35 -0700

    adds mapping for san bernadino

--- Code/Calc_FRID_summary2.R
+++ Code/Calc_FRID_summary2.R
@@ -3,14 +3,22 @@
 ################################################################################
 rm(list = ls())
 library(tidyverse); theme_set(theme_bw(base_size=9))
-library(rgdal)
+#library(rgdal)
+library(sf)
+library(ggspatial) # for scale bar and north arrow
 
-#Set workinng directory - change this file path to wherever you have FRID data
+#Set workinng directory - path to location with FRIDout.csv
 setwd("~/PMF/Details/SoCal/FRID_summaries")
 
 #Read in merged FRID data
 frid <- read.csv("FRIDout.csv")
 frid <- frid[!is.na(frid$AREA_GEO),] # cut slivers with 0 area
+
+# Read in shapefile
+map <- st_read("C:/GIS/FRID17/FRID_justFS.shp")
+
+# Read in FS boundary to make map prettier
+boundary <- st_read("C:/GIS/BasicOwnership_SoCalClip.shp")
 
 # Get standard data for each PFR
 PFRdata <- frid %>%
@@ -83,14 +91,15 @@ byCC2 <- byCC %>%
   summarize(Percent=sum(`% of area analyzed`))
 SB2 <- filter(byCC2, FORESTNAME=="San Bernardino National Forest") 
 
+
 # Make pie chart for each forest
 for (forest in Forests){
   f1 <- filter(byCC2, FORESTNAME==forest)
   p1 <- ggplot(data=f1, aes(x="", y=Percent,fill=`Condition Class`)) +
     geom_bar(stat="identity", color="black") +
     coord_polar("y", start=0) +
-    geom_text(aes(label = paste0(Percent, "%")), 
-              position = position_stack(vjust = 0.5)) +
+    geom_text(aes(x=1.55,label = paste0(Percent, "%")), 
+              position = position_stack(vjust = 0.5), color="black") +
     scale_fill_manual(name="Condition Class", 
                       values=c("red3","yellow","palegreen", "turquoise2","royalblue4"),
                       labels=c("3 (Too little fire)","2","1/-1","-2","-3 (Too much fire)")) +
@@ -106,10 +115,45 @@ for (forest in Forests){
          width = 7, height = 4.5, units = 'in')
 }
 
+# Make a map for each forest
+# trying out new dev version of ggplot2, which works with sf objects
+#devtools::install_github("tidyverse/ggplot2")
+library(ggplot2)
 
- 
- 
- # Could I do a sankey diagram?
+SB <- map %>% filter(FORESTNAME=="San Bernardino National Forest"&
+                       meanCC_FRI!=-999) %>%
+  mutate(meanCC=as.character(meanCC_FRI)) %>%
+  mutate(CC=ifelse(meanCC=="1"|meanCC=="-1", "1/-1", meanCC)) %>%
+  mutate(CC=factor(CC, ordered=T,
+                     levels=c("3","2","1/-1","-2", "-3")))
+B_SB <- filter(boundary, FORESTNAME=="San Bernardino National Forest" &
+                 OWNERCLASS=="USDA FOREST SERVICE")
+         
+ggplot(data=SB) +
+  geom_sf(aes(fill=CC), color=NA) +
+  theme_bw() +
+  scale_fill_manual(name="Condition Class", 
+        values=c("red3","yellow","palegreen", "turquoise2","royalblue4"),
+        labels=c("3 (Too little fire)","2","1/-1","-2","-3 (Too much fire)")) +
+   theme(axis.title.x = element_blank(),
+         axis.title.y = element_blank(),
+         axis.text.x = element_blank(),
+         axis.text.y = element_blank(),
+         axis.ticks = element_blank()) +
+  #       panel.grid.major = element_blank(),
+  #       panel.grid.minor = element_blank(),
+  #       legend.position = "right") +
+  #       #legend.key = element_blank())
+  geom_sf(data=B_SB, color="black", fill=NA) +
+  annotation_scale(location = "bl", width_hint = .2) +
+  annotation_north_arrow(location = "tr", which_north = "true", 
+                         pad_x = unit(0.09, "in"), pad_y = unit(0.09, "in"),
+                         height = unit(.3, "in"), width = unit(.3, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  ggtitle("2017 Percent Fire Return Interval Departures (PFRID): San Bernadino NF")
+
+
+# Could I do a sankey diagram?
  ggplot(as.data.frame(UCBAdmissions),
         aes(y = Freq, axis1 = Gender, axis2 = Dept)) +
    geom_alluvium(aes(fill = Admit), width = 1/12) +
